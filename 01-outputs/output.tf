afb3fdf6957ed2dea7621f5f0e4b92fa7996c290
eadesany <elijah.adesanya@oulu.fi>
2023-03-12 19:39:04 +0000

    Terraform commit

--- 01-outputs/output.tf
+++ 01-outputs/output.tf
@@ -6,6 +6,7 @@ data "aws_ami" "example" {
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
   owners      = [879970962635]
+
 }
   output "AMI_ID" {
     value = data.aws_ami.example.id
