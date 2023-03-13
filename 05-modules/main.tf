2655eaa16c52de1ef1382b9adca8bb74758da865
eadesany <elijah.adesanya@oulu.fi>
2023-03-13 19:32:21 +0000

    Terraform commit

--- 05-modules/main.tf
+++ 05-modules/main.tf
@@ -5,5 +5,12 @@ module "sg" {
 module "ec2" {
   source = "./ec2"
   sgid = module.sg.sgid
+  ami = "ami-0a017d8ceb274537d"
 }
 
+module "ec21" {
+  source = "./ec2"
+  sgid = module.sg.sgid
+  ami = "ami-09a0dac4253cfa03f
+  "
+}
